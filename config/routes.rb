992240b6f346ff15d34de3868eabec509872fe82
Giuliana Ferraro <giulianaferrarocamus@gmail.com>
2019-04-12 12:44:43 +0200

    update links

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,9 @@
 Rails.application.routes.draw do
-
-  root 'static_pages#home' # root to: "kittens#index" # kittens controller, index action (method)
+  root 'static_pages#home' 
+  # root to: "kittens#index" 
+  # => kittens controller, index action (method)
+  get  '/help',    to: 'static_pages#help'
+  get  '/about',   to: 'static_pages#about'
+  get  '/contact', to: 'static_pages#contact'
   
-  get 'static_pages/home'
-
-  get 'static_pages/help'
-
-  get 'static_pages/about'
-
-  get 'static_pages/contact'
 end

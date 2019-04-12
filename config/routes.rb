632a86d89ca0faf6865282508912a3c5bc94ab2b
Giuliana Ferraro <giulianaferrarocamus@gmail.com>
2019-04-12 11:48:10 +0200

    add styles

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  root 'static_pages#home'
+  root 'static_pages#home' # root to: "kittens#index" # kittens controller, index action (method)
   
   get 'static_pages/home'
 

3e1c2c73cdd585eece8aed946d2afa1564bd3391
Giuliana Ferraro <giulianaferrarocamus@gmail.com>
2019-04-16 18:33:33 +0200

    create show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
-  root 'static_pages#home' 
-  # root to: "kittens#index" 
-  # => kittens controller, index action (method)
+  root 'static_pages#home'
+  # get  'users/new'
   get  '/help',    to: 'static_pages#help'
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
   get  '/signup',  to: 'users#new'
-  
+  resources :users
 end
+
+# root to: "kittens#index" 
+# => kittens controller, index action (method)
